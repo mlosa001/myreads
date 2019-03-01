b1e8f7e33f77a2fcdac1a0f1f967f6805970c858
marysol <mlosa001@gmail.com>
2019-03-01 00:11:34 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Restaurant Review App (Stage 1)
+
+
+## Project Overview
+
+App to browse restaurant reviews. Part of Udacity's Fend Program. 
+
+## Installation
+
+- Download or clone this repo.
+- Navigate to the root folder using the Terminal.
+- Start a local HTTP server using Pythom.
+- check the version of Python you have: `python -V`
+- For Python 2.x, use `python -m SimpleHTTPServer 8000`
+- For Python 3.x, you can use `python3 -m http.server 8000`.
+- In Windows systems Python 3.x is already installed by default. To start a Python 3.x server, enter `python -m http.server 8000`.
+- When your server runs, open the site in your browser: `http://localhost:8000`.
+
+
+
+
+
+
---
+++ sw.js
@@ -0,0 +1,89 @@
+console.log('Service Worker: Registered');
+//
+self.addEventListener('install', function(e){
+	e.waitUntil(
+		caches.open('v1').then(function(cache){
+			return cache.addAll(cacheFiles);
+			return Promise.resolve("Dummy response to keep the console quiet");
+
+		})
+		);
+
+});
+
+const cacheFiles = [
+	'/',
+	'/index.html',
+	'/restaurant.html',
+	'/css/styles.css',
+	'/js/dbhelper.js',
+	'/js/main.js',
+	'/js/restaurant_info.js',
+	'/data/restaurants.json',
+	'/img/1.jpg',
+	'/img/2.jpg',
+	'/img/3.jpg',
+	'/img/4.jpg',
+	'/img/5.jpg',
+	'/img/6.jpg',
+	'/img/7.jpg',
+	'/img/8.jpg',
+	'/img/9.jpg',
+	'/img/10.jpg'
+];
+
+// self.addEventListener('fetch', function(e){
+	// e.respondWidth(
+	// 	caches.match(e.request).then(function(response){
+	// 		if(response){
+	// 			console.log('Found', e.request, ' in cache');
+	// 			return response;
+	// 		}0
+	// 		else{
+	// 			console.log('Could not find ', e.request, ' in cache, FETCHING!');
+	// 			return fetch(e.request);
+	// 			.then(function(response){
+	// 				const clonedResponse = response.clone();
+	// 			     caches.open('v1').then(function(cache){
+	// 			     	cache.put(e.request, clonedResponse);
+	// 			     })
+	// 			     return response;
+	// 			})
+	// 			.catch(function(err){
+	// 				console.error(err);
+	// 			})
+	// 		}
+	// 	})
+	// 	);
+// });
+
+self.addEventListener('fetch', function(e) {
+	e.respondWith(
+		caches.match(e.request).then(function(response){
+			if(response){
+				console.log('Found ', e.request, 'in the cache');
+
+				return response;
+			}
+			//fetch request but add it to cahce for later
+			else{
+				console.log('Not found' , e.request, ' in cache, Fethcing!');
+			    return fetch(e.request)
+			    //fetch item like normal
+				.then(function(response){
+					const clonedResp = response.clone();
+					caches.open('v1').then(function(cache){
+						cache.put(e.request, clonedResp);
+
+					})
+					return response;
+				})
+					.catch(function(err){
+						console.error(err);
+					});
+			}
+		})
+
+	);
+});
+
